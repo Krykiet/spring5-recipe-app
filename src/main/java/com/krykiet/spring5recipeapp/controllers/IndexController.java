1a1e0ff32c8ef836cce5e84a187b9f3d527d6609
Krykiet <chudzik.kd@gmail.com>
2022-10-22 12:58:58 +0200

    created index and index controller

---
+++ src/main/java/com/krykiet/spring5recipeapp/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.krykiet.spring5recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// Annotating Spring Component
+@Controller
+public class IndexController {
+
+    // URL values to call method
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String getIndexPage() {
+        return "index";
+    }
+}
