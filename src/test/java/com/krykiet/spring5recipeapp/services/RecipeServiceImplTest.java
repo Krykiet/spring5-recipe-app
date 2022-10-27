a68044995dff6c0717daa359d7ebb04dd2687146
Krystian Chudzik <chudzik.kd@gmail.com>
2022-10-27 14:32:50 +0200

    refactor

---
+++ src/test/java/com/krykiet/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,40 @@
+package com.krykiet.spring5recipeapp.services;
+
+import com.krykiet.spring5recipeapp.domain.Recipe;
+import com.krykiet.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() throws Exception {
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipesData = new HashSet<>();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+    }
+}
