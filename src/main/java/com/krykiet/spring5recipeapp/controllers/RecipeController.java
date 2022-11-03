94d7bd6dc00dacd58541124f4987f3e204683086
Krystian Chudzik <chudzik.kd@gmail.com>
2022-11-03 10:47:59 +0100

    added update recipe

--- src/main/java/com/krykiet/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/com/krykiet/spring5recipeapp/controllers/RecipeController.java
@@ -19,7 +19,7 @@ public class RecipeController {
     }
 
     // link address, {id} is passed as parameter
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model) {
         // content of the HTML in the model
         model.addAttribute("recipe", recipeService.findById(valueOf(id)));
@@ -35,12 +35,18 @@ public class RecipeController {
         return "recipe/recipeform";
     }
 
+    @RequestMapping("recipe/{id}/update")
+    public String updateRecipe(@PathVariable String id, Model model) {
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+        return "recipe/recipeform";
+    }
+
     // HTTP POST request
     @PostMapping
     @RequestMapping("recipe")
     public String saveOrUpdate(@ModelAttribute RecipeCommand command){
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
-        return "redirect:/recipe/show/" + savedCommand.getId();
+        return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 }
