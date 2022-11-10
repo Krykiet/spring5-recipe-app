1aeab2fabbafc1c55ac97278c3fc5b80958e8346
Krykiet <chudzik.kd@gmail.com>
2022-11-10 13:59:24 +0100

    added option for MySQL connection

--- src/main/java/com/krykiet/spring5recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/com/krykiet/spring5recipeapp/bootstrap/RecipeBootstrap.java
@@ -6,6 +6,7 @@ import com.krykiet.spring5recipeapp.repositories.RecipeRepository;
 import com.krykiet.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
@@ -20,6 +21,8 @@ import java.util.Optional;
  */
 @Slf4j
 @Component
+// gets initialized only when default profile is active
+@Profile("default")
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private final CategoryRepository categoryRepository;
